532b8fa9f4718d3fbf7b756f02222c6652295654
Ekaterina Egorova <arenel07@gmail.com>
2019-01-27 21:01:57 +0100

    Update and rename text_filter_oovs.py to utils/text_filter_oovs.py

--- utils/text_filter_oovs.py
+++ utils/text_filter_oovs.py
@@ -5,24 +5,10 @@ words_file = open(sys.argv[2], "r")
 output_file = open(sys.argv[3], "w")
 
 lex_words = []
-#ovs = []
 
 for line in words_file:
-#	print(line)
-#	if len(line) > 0:
-#	print line
-#	cur_transcr = ""
 	split_line = line.split()
-#	for part in split_line:
 	lex_words.append(split_line[0].lower())
-#	print(line[:line.index(" ")].encode("utf-8"))
-#	for i in range(1, len(split_line)):
-#		cur_transcr = cur_transcr + "phn_" + split_line[i] + " "
-#	cur_transcr = cur_transcr + "!sil"
-#	lex_transcr.append(cur_transcr)#(line[line.index(" ")+1:line.index("\n")])
-#	print(line[line.index(" ")+1:line.index("\n")].encode("utf-8"))
-
-#print(oovs)
 
 lines = 0
 oovs_num = 0
@@ -30,7 +16,6 @@ total_words = 0
 
 for line in text_file:
 	lines = lines + 1
-#	output_file.write(line)
 	split_line = line.split()
 	output_file.write(split_line[0] + " ")
 	for word in split_line[1:]:
